15e76c5520924b65d2a567af07b7996f67856307
asmagacz <29546@student.pwsztar.edu.pl>
2019-03-08 16:22:41 +0100

    added navbar

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav = '<li class="nav-item"><a href="index.html" class="nav-link active" href="#">Home</a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="#">Quiz</a></li>' +
+    '<li class="nav-item"><a href="contact.html" class="nav-link" href="#">Kontakt</a></li>';
+
+let elem = document.querySelector(".navbar");
+elem.innerHTML = '<ul class="nav justify-content-center">' + nav  + '</ul>';
