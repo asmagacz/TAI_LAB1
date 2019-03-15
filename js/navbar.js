1358adf8cc46392d3aad65795c1acf1054dee4b5
asmagacz <29546@student.pwsztar.edu.pl>
2019-03-15 16:43:55 +0100

    added questions

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 let nav = '<li class="nav-item"><a href="index.html" class="nav-link active" href="#">Home</a></li>' +
     '<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>' +
-    '<li class="nav-item"><a class="nav-link" href="#">Quiz</a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="quiz.html">Quiz</a></li>' +
     '<li class="nav-item"><a href="contact.html" class="nav-link" href="#">Kontakt</a></li>';
 
 let elem = document.querySelector(".navbar");
